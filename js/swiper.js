d48c719a30bf3fa7547d4a9100ebe87db91b6536
Gabriel Fonseca <gabrielfonseca1811@gmail.com>
2025-01-22 09:51:02 -0300

    correção js - swiper

--- js/swiper.js
+++ js/swiper.js
@@ -1,14 +1,14 @@
-var swiper = new Swiper('.swiper-container',{
+var swiper = new Swiper('.swiper-container', {
   slidesPerView: 'auto',
-  spaceBeteween: 40,
+  spaceBetween: 40, // Corrigido também o typo em "spaceBetween"
   grabCursor: true,
   pagination: {
     el: '.swiper-pagination',
     clickable: true,
     type: 'progressbar',
   },
-  navigation:{
-    nexEL: '.button-next',
-    prevEL: '.button-prev',
+  navigation: {
+    nextEl: '.button-next', // Corrigido "nexEL" para "nextEl"
+    prevEl: '.button-prev', // Corrigido "prevEL" para "prevEl"
   },
 });
